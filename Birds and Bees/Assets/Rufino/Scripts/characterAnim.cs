2d5093372fdda47da70eeca6a1e0c6d5b27c3d58
Rufino Ricardo Ansara <rufinoansara@gmail.com>
2016-01-29 22:15:53 -0500

    First one

---
+++ Birds and Bees/Assets/Rufino/Scripts/characterAnim.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterAnim : MonoBehaviour {
+	
+	
+	public Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+		
+		anim = this.GetComponent<Animator>();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			
+			
+			anim.SetBool("Dance", true);
+				
+				
+		} else {
+			anim.SetBool("Dance", false);
+		}
+
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			
+			anim.SetBool("React", true);
+			
+			
+		} else {
+			anim.SetBool("React", false);
+		}
+		
+	}
+}
