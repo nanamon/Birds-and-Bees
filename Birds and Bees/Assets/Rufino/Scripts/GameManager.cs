43b891817904adeb99f0596242748e535a52231b
Rufino Ricardo Ansara <rufinoansara@gmail.com>
2016-01-29 23:21:25 -0500

    Game Master File
    Added

---
+++ Birds and Bees/Assets/Rufino/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+
+
+	//0 is NO player selected
+	public static int playerOne = 0;
+	public static int playerTwo = 0;
+	public static int playerThree = 0;
+	public static bool playerFour = 0;
+
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
