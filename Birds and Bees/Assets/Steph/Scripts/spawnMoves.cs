9d60198cadc6dc6c3506d6f6cdf56ba3dfe9ede7
stephonatron <thestephonatron@gmail.com>
2016-01-29 23:18:20 -0500

    More arrows n shit

--- Birds and Bees/Assets/Steph/Scripts/spawnMoves.cs
+++ Birds and Bees/Assets/Steph/Scripts/spawnMoves.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class spawnMoves : MonoBehaviour {
 
+	GameObject arrow;
+	GameObject[] moves;
+
+	float speed = 0.05f;
+
 	// Use this for initialization
 	void Start () {
+		spawnMove ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//cycle through array to update positions
+		arrow.transform.Translate(speed, 0, 0);
+	}
 
+	void spawnMove()
+	{
+		arrow = Instantiate (Resources.Load("arrow"), new Vector3(-10, 0, 0), Quaternion.identity)as GameObject;
 	}
 }
